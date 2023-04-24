ff49c4c777951aeeee995acd6cc0fd508e18d8a9
paramighty <avinnosatta@gmail.com>
2023-04-24 16:59:16 +0200

    CSS done for the 4 main buttons: Edit, Delete, Add New Task, and I;m Out

--- client/src/components/ListItem.js
+++ client/src/components/ListItem.js
@@ -1,8 +1,20 @@
 const ListItem = ({ task }) => {
   return (
-    <div className="list-item">
-      <p>{task.title}</p>
-    </div>
+    <li className="list-item">
+      <div className="info-container">
+        <p className="task-title">
+          {task.title}
+        </p>
+        <div className="button-container">
+          <button className="edit">
+            EDIT
+          </button>
+          <button className="delete">
+            DELETE
+          </button>
+        </div>
+      </div>
+    </li>
   );
 };
 
